4d0b550b451786bea162d24970f3137ab94a167c
MasoudAndiwal <Masoudandiwal89@gmail.com>
2025-06-11 13:04:19 +0430

    create the navbar , navbaritems

---
+++ components/navitems.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react'
+const Navitem = [{label:'Home', href:'/'}, {label:'Companions', href:'/companions'}, {label:'My Journey', href:'/my-journey'}, {label:'Sign In', href:'/sign-in    '}] ;
+
+const Navitems = () => {
+    const pathname = usePathname();
+  return (
+    <nav className='flex items-center gap-4'>
+    {Navitem.map((item) => (
+        <Link key={item.label} href={item.href} className={cn(pathname === item.href && 'text-primary font-semibold')}>
+            {item.label}
+        </Link>
+    ))}
+    </nav>
+  )
+}
+
+export default Navitems
